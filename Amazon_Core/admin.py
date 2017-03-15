d48540a472a63717acc25c48ba305e18ed217104
ADMIN <jiteesh7@gmail.com>
2017-03-15 17:30:19 -0500

    changes

--- Amazon_Core/admin.py
+++ Amazon_Core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CustomerProfile, BillingAddress, ShippingAddress, CreditCard, Order,Shipment, Item,LineItem
+from .models import CustomerProfile, BillingAddress, ShippingAddress, CreditCard, Order,Shipment, Item, LineItem
 
 
 
